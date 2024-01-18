bf5325fa32bc06fad7f599953ac54a2a8dc70143
Radalls <leosallard@gmail.com>
2024-01-18 18:34:16 +0100

    core: Inventory System - object parameters + refacto

--- src/engine/systems/inventory.ts
+++ src/engine/systems/inventory.ts
@@ -2,25 +2,31 @@ import { Inventory } from "../components/inventory";
 import { Item } from "../components/item";
 import { getComponent } from "../entities/entity.manager";
 
-const itemNoAmount = (item: Item): boolean => item._amount <= 0;
-const itemNoName = (item: Item): boolean => !(item.info?._name);
+const itemAmountError = (item: Item): boolean => item._amount <= 0 || item._maxAmount <= 0 || item._amount > item._maxAmount;
+const itemNameError = (item: Item): boolean => !(item.info?._name) || item.info?._name === '';
 
-const findItemByName = (inventory: Inventory, itemName: Item['info']['_name']): Item | undefined =>
+const findItemByName = ({ inventory, itemName }: {
+    inventory: Inventory,
+    itemName: string,
+}): Item | undefined =>
     inventory.items.find(item => item.info?._name === itemName);
 
 /**
  * Adds an item to an inventory.
- * @param entity needs to have an inventory
+ * @param entityId needs to have an inventory
  * @param item   needs to have an amount and a name
  * @returns whether the item was added to the inventory
  */
-export const addItemToInventory = (entity: number, item: Item): boolean => {
-    if (itemNoAmount(item) || itemNoName(item)) {
+export const addItemToInventory = ({ entityId, item }: {
+    entityId: number,
+    item: Item,
+}): boolean => {
+    if (itemAmountError(item) || itemNameError(item)) {
         throw new Error(`Item ${item} is invalid`);
     }
 
-    const inventory = getComponent(entity, 'Inventory');
-    const itemInInventory = findItemByName(inventory, item.info?._name);
+    const inventory = getComponent({ entityId, componentId: 'Inventory' });
+    const itemInInventory = findItemByName({ inventory, itemName: item.info?._name });
 
     if (itemInInventory) {
         if (itemInInventory._amount + item._amount <= itemInInventory._maxAmount) {
@@ -29,10 +35,10 @@ export const addItemToInventory = (entity: number, item: Item): boolean => {
             const amountRemaining = itemInInventory._amount + item._amount - itemInInventory._maxAmount;
             itemInInventory._amount = itemInInventory._maxAmount;
 
-            addItemToInventory(
-                entity,
-                { ...item, _amount: amountRemaining }
-            );
+            addItemToInventory({
+                entityId,
+                item: { ...item, _amount: amountRemaining },
+            });
         }
     } else {
         if (inventory.items.length < inventory._nbItems) {
@@ -52,9 +58,13 @@ export const addItemToInventory = (entity: number, item: Item): boolean => {
  * @param itemAmount amount of the item to remove
  * @returns whether the item was removed from the inventory
  */
-export const removeItemFromInventory = (entity: number, itemName: string, itemAmount: number): boolean => {
-    const inventory = getComponent(entity, 'Inventory');
-    const slotWithItem = findItemByName(inventory, itemName);
+export const removeItemFromInventory = ({ entityId, itemName, itemAmount }: {
+    entityId: number,
+    itemName: string,
+    itemAmount: number,
+}): boolean => {
+    const inventory = getComponent({ entityId, componentId: 'Inventory' });
+    const slotWithItem = findItemByName({ inventory, itemName });
 
     if (slotWithItem && slotWithItem._amount >= itemAmount) {
         slotWithItem._amount -= itemAmount;
