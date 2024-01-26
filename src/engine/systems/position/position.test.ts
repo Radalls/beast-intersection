85976c3cdf69529771090a44728d7a90edd07fad
Radalls <leosallard@gmail.com>
2024-01-26 12:07:57 +0100

    feat: create Position System

---
+++ src/engine/systems/position/position.test.ts
@@ -0,0 +1,61 @@
+import { Position } from "../../components/position";
+import { addComponent, createEntity } from "../../entities/entity.manager";
+import { updatePosition } from "./position";
+
+describe('Position System', () => {
+    const entityId = createEntity();
+    const position: Position = {
+        _: 'Position',
+        _x: 0,
+        _y: 0,
+    };
+
+    beforeAll(() => {
+        addComponent({
+            entityId,
+            component: position,
+        });
+    });
+
+    beforeEach(() => {
+        position._x = 0;
+        position._y = 0;
+    });
+
+    describe(updatePosition.name, () => {
+        test('Should be a function', () => {
+            expect(typeof updatePosition).toBe('function');
+        });
+
+        test('Should throw if parameters are invalid', () => {
+            expect(() => updatePosition({
+                entityId,
+                x: 0,
+                y: 0,
+            })).toThrow();
+
+            expect(() => updatePosition({
+                entityId,
+                x: -5,
+                y: 0,
+            })).toThrow();
+
+            expect(() => updatePosition({
+                entityId,
+                x: 0,
+                y: -5,
+            })).toThrow();
+        });
+
+        test('Should update position', () => {
+            updatePosition({
+                entityId,
+                x: 5,
+                y: 5,
+            });
+
+            expect(position._x).toBe(5);
+            expect(position._y).toBe(5);
+        });
+    });
+});
---
+++ src/engine/systems/position/position.ts
@@ -0,0 +1,28 @@
+import { getComponent } from "../../entities/entity.manager";
+
+const invalidPosition = (x: number, y: number) => x < 0 || y < 0;
+const samePosition = ({ entityPosition, inputPosition }: {
+    entityPosition: { x: number, y: number },
+    inputPosition: { x: number, y: number },
+}) => entityPosition.x === inputPosition.x && entityPosition.y === inputPosition.y
+
+export const updatePosition = ({ entityId, x, y }: {
+    entityId: number,
+    x: number,
+    y: number,
+}) => {
+    if (invalidPosition(x, y)) {
+        throw new Error(`Position (${x}-${y}) is invalid`);
+    }
+
+    const position = getComponent({ entityId, componentId: 'Position' });
+    if (samePosition({
+        entityPosition: { x: position._x, y: position._y },
+        inputPosition: { x, y },
+    })) {
+        throw new Error(`Position (${x}-${y}) is invalid`);
+    }
+
+    position._x = x;
+    position._y = y;
+}
