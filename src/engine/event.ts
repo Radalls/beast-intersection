b8fb7bcd1850bf8a3aef43d56908905a3b35d14d
Radalls <leosallard@gmail.com>
2024-01-26 12:09:19 +0100

    feat: create Event Manager

---
+++ src/engine/event.ts
@@ -0,0 +1,33 @@
+import { getComponent } from "./entities/entity.manager";
+import { getGameEntity } from "./entity";
+import { updatePosition } from "./systems/position/position";
+
+export enum InputKeys {
+    UP = 'z',
+    DOWN = 'd',
+    LEFT = 'q',
+    RIGHT = 's',
+}
+
+export enum Events {
+    ENTITY_POSITION_UPDATE = 'ENTITY_POSITION_UPDATE',
+}
+
+export const onInputKeyDown = (inputKey: string) => {
+    if (inputKey === InputKeys.UP) {
+        const playerEntity = getGameEntity('Player');
+        const playerPosition = getComponent({ entityId: playerEntity, componentId: 'Position' });
+
+        updatePosition({
+            entityId: playerEntity,
+            x: playerPosition._x,
+            y: playerPosition._y - 1,
+        });
+
+        emit(Events.ENTITY_POSITION_UPDATE, { entityName: 'Player', x: playerPosition._x, y: playerPosition._y });
+    }
+};
+
+export const emit = (eventName: string, data: Object) => {
+    console.log(`Event ${eventName} emitted with data ${JSON.stringify(data)}`);
+};
