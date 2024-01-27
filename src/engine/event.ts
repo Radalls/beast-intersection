3192816b14beced3a2bfcab8932d3e3b65f127e8
Radalls <leosallard@gmail.com>
2024-01-27 15:22:04 +0100

    feat: Event Manager - update keydown movements

--- src/engine/event.ts
+++ src/engine/event.ts
@@ -14,18 +14,42 @@ export enum Events {
     ENTITY_POSITION_UPDATE = 'ENTITY_POSITION_UPDATE',
 }
 
-export const onInputKeyDown = (inputKey: string) => {
-    if (inputKey === InputKeys.UP) {
-        const playerEntity = getGameEntity('Player');
-        const playerPosition = getComponent({ entityId: playerEntity, componentId: 'Position' });
+export const onInputKeyDown = (inputKey: InputKeys) => {
+    const playerEntity = getGameEntity('Player');
+    const playerPosition = getComponent({ entityId: playerEntity, componentId: 'Position' });
 
-        updatePosition({
-            entityId: playerEntity,
-            x: playerPosition._x,
-            y: playerPosition._y - 1,
-        });
+    try {
+        if (inputKey === InputKeys.UP) {
+            updatePosition({
+                entityId: playerEntity,
+                x: playerPosition._x,
+                y: playerPosition._y - 1,
+            });
+        }
+        if (inputKey === InputKeys.DOWN) {
+            updatePosition({
+                entityId: playerEntity,
+                x: playerPosition._x,
+                y: playerPosition._y + 1,
+            });
+        }
+        if (inputKey === InputKeys.LEFT) {
+            updatePosition({
+                entityId: playerEntity,
+                x: playerPosition._x - 1,
+                y: playerPosition._y,
+            });
+        }
+        if (inputKey === InputKeys.RIGHT) {
+            updatePosition({
+                entityId: playerEntity,
+                x: playerPosition._x + 1,
+                y: playerPosition._y,
+            });
+        }
 
         emit(Events.ENTITY_POSITION_UPDATE, { entityName: 'Player', x: playerPosition._x, y: playerPosition._y });
+    } catch (e) {
+        console.error(e);
     }
 };
-
