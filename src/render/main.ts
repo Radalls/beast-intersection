43a51d2c4eea549e5d24779b2899bbe99ed3b9c6
Radalls <leosallard@gmail.com>
2024-02-01 21:58:09 +0100

    feat: add tile size reference

--- src/render/main.ts
+++ src/render/main.ts
@@ -2,6 +2,8 @@ import { onInputKeyDown } from "./event";
 
 export const app = document.getElementById("app")!;
 
+export const TILE_PIXEL_SIZE = 64;
+
 export const main = () => {
     document.addEventListener("keydown", onInputKeyDown);
 };
