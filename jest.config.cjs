8813aacb634fa05909ecefe784a1075af39bba76
Radalls <leosallard@gmail.com>
2024-02-09 22:29:53 +0100

    core: update jest dependencies

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,25 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'jsdom',
+    testMatch: ['**/*.test.ts'],
+    modulePathIgnorePatterns: ["<rootDir>/src/render"],
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                diagnostics: {
+                    ignoreCodes: [1343]
+                },
+                astTransformers: {
+                    before: [
+                        {
+                            path: 'node_modules/ts-jest-mock-import-meta',
+                            options: { metaObjectReplacement: { url: 'https://www.url.com' } }
+                        }
+                    ]
+                }
+            }
+        ],
+    },
 };
