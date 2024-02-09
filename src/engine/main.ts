dd2445c91c9067f76660c2bb244541741a5524d0
Radalls <leosallard@gmail.com>
2024-02-09 22:42:39 +0100

    feat: Engine Manager - add tile management

--- src/engine/main.ts
+++ src/engine/main.ts
@@ -1,4 +1,4 @@
-import { generateTiles } from './systems/tilemap/tilemap';
+import { generateTiles, setTile } from './systems/tilemap/tilemap';
 import { createEntity } from "./entities/entity.manager";
 import { addInventory, addPosition, addSprite, addTileMap } from "./systems/component/component";
 
@@ -8,7 +8,9 @@ export const main = () => {
     generateTiles(tilemapEntityId);
 
     const playerEntityId = createEntity('Player');
-    addPosition({ entityId: playerEntityId });
     addSprite({ entityId: playerEntityId, height: 2, image: 'player.png' });
+    addPosition({ entityId: playerEntityId });
     addInventory({ entityId: playerEntityId, maxSlots: 20 });
+
+    setTile({ tilemapEntityId, entityId: playerEntityId });
 };
