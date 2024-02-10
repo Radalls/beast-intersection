ada7e170eaa49add868b2f8d54f73b39e111f6b7
Radalls <leosallard@gmail.com>
2024-02-10 23:50:45 +0100

    feat: Engine - add resource stick

--- src/engine/main.ts
+++ src/engine/main.ts
@@ -1,6 +1,6 @@
 import { generateTiles, setTile } from './systems/tilemap/tilemap';
 import { createEntity } from "./entities/entity.manager";
-import { addInventory, addPosition, addSprite, addTileMap } from "./systems/component/component";
+import { addInventory, addPosition, addResource, addSprite, addTileMap, addTrigger } from "./systems/component/component";
 
 export const main = () => {
     const tilemapEntityId = createEntity('TileMap');
@@ -13,4 +13,16 @@ export const main = () => {
     addInventory({ entityId: playerEntityId, maxSlots: 20 });
 
     setTile({ tilemapEntityId, entityId: playerEntityId });
+
+    const stickEntityId = createEntity('ResourceStick');
+    addSprite({ entityId: stickEntityId, image: 'resource_stick.png' });
+    addPosition({ entityId: stickEntityId, x: 3, y: 3 });
+    addResource({
+        entityId: stickEntityId,
+        isTemporary: true,
+        itemName: 'Stick',
+    });
+    addTrigger({ entityId: stickEntityId });
+
+    setTile({ tilemapEntityId, entityId: stickEntityId });
 };
