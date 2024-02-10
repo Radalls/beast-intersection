5ebf0066a631f34b533113fb185788f6cdf533d5
Radalls <leosallard@gmail.com>
2024-02-10 23:52:15 +0100

    fix: Render Event - fix event typing

--- src/render/event.ts
+++ src/render/event.ts
@@ -1,60 +1,48 @@
+import { Event } from './../engine/event';
 import { Position } from "../engine/components/position";
 import { Sprite } from "../engine/components/sprite";
 import { Tile, TileMap } from "../engine/components/tilemap";
 import { EventTypes, onInputKeyDown as engineOnInputKeyDown } from "../engine/event";
-import { createEntity, updateSprite, updatePosition, createTileMap, createTileMapTile } from "./template";
+import { createEntity, createSprite, updatePosition, createTileMap, createTileMapTile } from "./template";
 
 export const onInputKeyDown = (e: any) => { engineOnInputKeyDown(e.key); };
 
-export const event = ({ type, entityId, data }: {
-    type: EventTypes,
-    entityId: string,
-    data?: Object,
-}) => {
-    console.log(`[${type}] - ${entityId}: ${JSON.stringify(data)}`);
-
-    if (type === EventTypes.ENTITY_CREATE) {
-        onEntityCreate({ entityId });
+export const event = (event: Event) => {
+    if (event.type === EventTypes.ENTITY_CREATE) {
+        onEntityCreate({ entityId: event.entityId });
     }
-    else if (type === EventTypes.ENTITY_INVENTORY_CREATE) {
-        onEntityInventoryCreate({ entityId });
+    else if (event.type === EventTypes.ENTITY_INVENTORY_CREATE) {
+        onEntityInventoryCreate({ entityId: event.entityId });
     }
-    else if (type === EventTypes.ENTITY_POSITION_CREATE) {
-        onEntityPositionCreate({
-            entityId,
-            position: data as Omit<Position, '_'>,
-        });
-    }
-    else if (type === EventTypes.ENTITY_POSITION_UPDATE) {
+    else if (event.type === EventTypes.ENTITY_POSITION_UPDATE && event.data) {
         onEntityPositionUpdate({
-            entityId,
-            position: data as Omit<Position, '_'>,
-        });
-    }
-    else if (type === EventTypes.ENTITY_SPRITE_CREATE) {
-        onEntitySpriteCreate({
-            entityId,
-            sprite: data as Omit<Sprite, '_'>,
-        });
-    }
-    else if (type === EventTypes.TILEMAP_CREATE) {
-        onTileMapCreate({
-            entityId,
-            tilemap: data as Omit<TileMap, '_' | 'tiles'>,
-        });
-    }
-    else if (type === EventTypes.TILEMAP_TILE_CREATE) {
-        onTileMapTileCreate({
-            entityId,
-            tile: data as {
-                positionX: Tile['position']['_x'],
-                positionY: Tile['position']['_y'],
-                sprite: Tile['sprite']['_image'],
+            entityId: event.entityId,
+            data: event.data as {
+                position: Pick<Position, '_x' | '_y'>,
+                sprite: Pick<Sprite, '_height' | '_width'>,
             },
         });
     }
+    else if (event.type === EventTypes.ENTITY_SPRITE_CREATE && event.data) {
+        onEntitySpriteCreate({
+            entityId: event.entityId,
+            sprite: event.data as Pick<Sprite, '_height' | '_image' | '_width'>,
+        });
+    }
+    else if (event.type === EventTypes.TILEMAP_CREATE && event.data) {
+        onTileMapCreate({
+            entityId: event.entityId,
+            tilemap: event.data as Pick<TileMap, '_height' | '_width'>,
+        });
+    }
+    else if (event.type === EventTypes.TILEMAP_TILE_CREATE && event.data) {
+        onTileMapTileCreate({
+            entityId: event.entityId,
+            tile: event.data as Pick<Tile, 'position' | 'sprite'>,
+        });
+    }
     else {
-        throw new Error(`Unknown event type: ${type}`);
+        throw new Error(`Unknown event type: ${event.type}`);
     }
 };
 
@@ -70,41 +58,33 @@ const onEntityInventoryCreate = ({ entityId }: {
     console.log('onEntityInventoryCreate', entityId);
 }
 
-const onEntityPositionCreate = ({ entityId, position }: {
+const onEntityPositionUpdate = ({ entityId, data: { position, sprite } }: {
     entityId: string,
-    position: Omit<Position, '_'>
+    data: {
+        position: Pick<Position, '_x' | '_y'>,
+        sprite: Pick<Sprite, '_height' | '_width'>,
+    },
 }) => {
-    updatePosition({ entityId, position });
-}
-
-const onEntityPositionUpdate = ({ entityId, position }: {
-    entityId: string,
-    position: Omit<Position, '_'>
-}) => {
-    updatePosition({ entityId, position });
+    updatePosition({ entityId, position, sprite });
 };
 
 const onEntitySpriteCreate = ({ entityId, sprite }: {
     entityId: string,
-    sprite: Omit<Sprite, '_'>
+    sprite: Pick<Sprite, '_height' | '_image' | '_width'>,
 }) => {
-    updateSprite({ entityId, sprite });
+    createSprite({ entityId, sprite });
 };
 
 const onTileMapCreate = ({ entityId, tilemap }: {
     entityId: string,
-    tilemap: Omit<TileMap, '_' | 'tiles'>
+    tilemap: Pick<TileMap, '_height' | '_width'>,
 }) => {
     createTileMap({ entityId, tilemap });
 };
 
 const onTileMapTileCreate = ({ entityId, tile }: {
     entityId: string,
-    tile: {
-        positionX: Tile['position']['_x'],
-        positionY: Tile['position']['_y'],
-        sprite: Tile['sprite']['_image'],
-    },
+    tile: Pick<Tile, 'position' | 'sprite'>,
 }) => {
     createTileMapTile({ entityId, tile });
 };
