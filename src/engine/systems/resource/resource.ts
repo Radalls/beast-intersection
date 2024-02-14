deafbc53562f91421e7c9fbc9a22deca5bc5f4d5
Radalls <leosallard@gmail.com>
2024-02-14 10:29:48 +0100

    feat: Resource System - destroy trigger when temp

--- src/engine/systems/resource/resource.ts
+++ src/engine/systems/resource/resource.ts
@@ -1,10 +1,20 @@
-import { destroyEntity, getComponent } from "../../entities/entity.manager";
+import { checkComponent, destroyEntity, getComponent } from "../../entities/entity.manager";
+import { getPlayer } from "../../store";
 import { addItemToInventory } from "../inventory/inventory";
+import { destroyTrigger } from "../trigger/trigger";
 
 export const useResource = ({ entityId, triggeredEntityId }: {
-    entityId: string,
+    entityId?: string | null,
     triggeredEntityId: string,
 }) => {
+    entityId = entityId || getPlayer();
+    if (!(entityId)) {
+        throw {
+            message: `Entity does not exist`,
+            where: useResource.name,
+        }
+    }
+
     const resource = getComponent({ entityId: triggeredEntityId, componentId: 'Resource' });
 
     addItemToInventory({
@@ -14,6 +24,10 @@ export const useResource = ({ entityId, triggeredEntityId }: {
     });
 
     if (resource._isTemporary) {
+        const resourceTrigger = checkComponent({ entityId: triggeredEntityId, componentId: 'Trigger' });
+        if (resourceTrigger) {
+            destroyTrigger({ entityId: triggeredEntityId });
+        }
         destroyEntity({ entityId: triggeredEntityId });
     }
 };
