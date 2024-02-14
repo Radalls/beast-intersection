050659c53ce7ff9616b04dd70244096621a976a6
Radalls <leosallard@gmail.com>
2024-02-14 10:28:56 +0100

    feat: Engine Main - use store params

--- src/engine/main.ts
+++ src/engine/main.ts
@@ -1,17 +1,18 @@
 import { generateTiles, setTile } from './systems/tilemap/tilemap';
 import { createEntity } from "./entities/entity.manager";
 import { addInventory, addPosition, addResource, addSprite, addTileMap, addTrigger } from "./systems/component/component";
+import { TILEMAP_ENTITY_ID, PLAYER_ENTITY_ID } from './entities/entity';
 
 export const main = () => {
-    const tilemapEntityId = createEntity('TileMap');
+    const tilemapEntityId = createEntity(TILEMAP_ENTITY_ID);
     addTileMap({ entityId: tilemapEntityId });
-    generateTiles(tilemapEntityId);
+    generateTiles({});
 
-    const playerEntityId = createEntity('Player');
+    const playerEntityId = createEntity(PLAYER_ENTITY_ID);
     addSprite({ entityId: playerEntityId, height: 2, image: 'player.png' });
     addPosition({ entityId: playerEntityId });
     addInventory({ entityId: playerEntityId, maxSlots: 20 });
-    setTile({ tilemapEntityId, entityId: playerEntityId });
+    setTile({ entityId: playerEntityId });
 
     const stickEntityId = createEntity('ResourceStick');
     addSprite({ entityId: stickEntityId, image: 'resource_stick.png' });
@@ -22,5 +23,5 @@ export const main = () => {
         itemName: 'Stick',
     });
     addTrigger({ entityId: stickEntityId });
-    setTile({ tilemapEntityId, entityId: stickEntityId });
+    setTile({ entityId: stickEntityId });
 };
