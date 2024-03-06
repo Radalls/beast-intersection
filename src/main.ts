f9055a10df3b6057380e065796c426a698603a5f
Radalls <leosallard@gmail.com>
2024-03-06 20:57:36 +0100

    core: update css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { main as engineMain } from "./engine/main";
 import { main as renderMain } from "./render/main";
 
+import './render/styles/activity.css';
 import './render/styles/inventory.css';
+import './render/styles/tilemap.css';
 
 renderMain();
 engineMain();
