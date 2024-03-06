0b3a631685f544d8065cef0fb17626db8e7dbf77
Radalls <leosallard@gmail.com>
2024-03-06 21:27:38 +0100

    feat: Resource System - update tests

--- src/engine/systems/resource/resource.test.ts
+++ src/engine/systems/resource/resource.test.ts
@@ -1,7 +1,7 @@
 import { Inventory } from "../../components/inventory";
-import { Resource } from "../../components/resource";
-import { addComponent, createEntity, getEntity } from "../../entities/entity.manager";
-import { useResource } from "./resource";
+import { Resource, ActivityTypes } from "../../components/resource";
+import { addComponent, createEntity, getEntity } from "../../services/entity";
+import { destroyResource, getResourceItem, useResource } from "./resource";
 
 jest.mock('../../../render/event.ts', () => ({
     event: jest.fn(),
@@ -11,6 +11,7 @@ describe('Resource System', () => {
     const resourceEntityId = createEntity('Resource');
     const resource: Resource = {
         _: 'Resource',
+        _activityType: ActivityTypes.PICKUP,
         _isTemporary: false,
         item: {
             info: { _name: 'Stick' },
@@ -38,37 +39,74 @@ describe('Resource System', () => {
     });
 
     describe(useResource.name, () => {
-        beforeEach(() => {
-            playerInventory.slots = [];
-        });
-
         test('Should be a function', () => {
             expect(typeof useResource).toBe('function');
         });
 
-        test('Should add resource to player inventory', () => {
-            useResource({
-                entityId: playerEntityId,
-                triggeredEntityId: resourceEntityId,
-            });
-
-            expect(playerInventory.slots).toHaveLength(1);
-            expect(playerInventory.slots[0].item.info._name).toBe('Stick');
-        });
-
-        test('Should destroy resource entity if temporary', () => {
+        test('Should use temp pickup resource', () => {
             const temporaryResourceEntityId = createEntity('TemporaryResource');
 
             addComponent({
                 entityId: temporaryResourceEntityId,
                 component: { ...resource, _isTemporary: true },
             });
+
             useResource({
                 entityId: playerEntityId,
-                triggeredEntityId: temporaryResourceEntityId,
+                resourceEntityId: temporaryResourceEntityId,
             });
 
+            expect(playerInventory.slots).toHaveLength(1);
             expect(getEntity(temporaryResourceEntityId)).toBeNull();
+        });
+
+        test('Should use bug resource', () => {
+            const bugResourceEntityId = createEntity('ResourceBug');
+
+            addComponent({
+                entityId: bugResourceEntityId,
+                component: { ...resource, _activityType: ActivityTypes.BUG },
+            });
+
+            useResource({
+                entityId: playerEntityId,
+                resourceEntityId: bugResourceEntityId,
+            });
+
+            expect(getEntity(bugResourceEntityId)).not.toBeNull();
+        });
+    });
+
+    describe(getResourceItem.name, () => {
+        beforeEach(() => {
+            playerInventory.slots = [];
+        });
+
+        test('Should be a function', () => {
+            expect(typeof getResourceItem).toBe('function');
+        });
+
+        test('Should add resource item to player inventory', () => {
+            getResourceItem({
+                entityId: playerEntityId,
+                resourceEntityId,
+            });
+
+            expect(playerInventory.slots).toHaveLength(1);
+            expect(playerInventory.slots[0]._amount).toBe(1);
+            expect(playerInventory.slots[0].item.info._name).toBe('Stick');
+        });
+    });
+
+    describe(destroyResource.name, () => {
+        test('Should be a function', () => {
+            expect(typeof destroyResource).toBe('function');
+        });
+
+        test('Should destroy resource entity', () => {
+            destroyResource({ resourceEntityId: resourceEntityId });
+
+            expect(getEntity(resourceEntityId)).toBeNull();
         });
     });
 });
