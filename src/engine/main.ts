40a1af4b3915d28eee9903274d9cbccf0857645b
Radalls <leosallard@gmail.com>
2024-03-06 21:05:35 +0100

    feat: Engine - add cycle + bug entity

--- src/engine/main.ts
+++ src/engine/main.ts
@@ -1,9 +1,17 @@
 import { generateTiles, setTile } from './systems/tilemap/tilemap';
-import { createEntity } from "./entities/entity.manager";
-import { addInventory, addPosition, addResource, addSprite, addTileMap, addTrigger } from "./systems/component/component";
-import { TILEMAP_ENTITY_ID, PLAYER_ENTITY_ID } from './entities/entity';
+import { PLAYER_ENTITY_ID, TILEMAP_ENTITY_ID, createEntity } from "./services/entity";
+import { addInventory, addPosition, addResource, addSprite, addTileMap, addTrigger } from "./services/component";
+import { ActivityTypes } from './components/resource';
+import { setState } from './state';
+import { getCycle, runCycle } from './cycle';
 
 export const main = () => {
+    setState('isGameRunning', true);
+
+    setInterval(() => {
+        runCycle();
+    }, getCycle('deltaTime') * 1000);
+
     const tilemapEntityId = createEntity(TILEMAP_ENTITY_ID);
     addTileMap({ entityId: tilemapEntityId });
     generateTiles({});
@@ -24,4 +32,23 @@ export const main = () => {
     });
     addTrigger({ entityId: stickEntityId });
     setTile({ entityId: stickEntityId });
+
+    const butterflyEntityId = createEntity('ResourceButterfly');
+    addSprite({ entityId: butterflyEntityId, image: 'resource_butterfly.png' });
+    addPosition({ entityId: butterflyEntityId, x: 4, y: 6 });
+    addResource({
+        entityId: butterflyEntityId,
+        activityType: ActivityTypes.BUG,
+        isTemporary: true,
+        activityData: {
+            _hp: 3,
+            _maxHp: 3,
+            _maxNbErrors: 3,
+            _nbErrors: 0,
+            _symbolInterval: 3,
+        },
+        itemName: 'Butterfly',
+    });
+    addTrigger({ entityId: butterflyEntityId });
+    setTile({ entityId: butterflyEntityId });
 };
