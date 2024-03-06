dac63d09ed2b5d25629b88734ccc39932550abcf
Radalls <leosallard@gmail.com>
2024-03-06 21:07:48 +0100

    feat: Engine State - refacto to generic

--- src/engine/state.ts
+++ src/engine/state.ts
@@ -1,7 +1,10 @@
-export const state = {
-    isMenuOpen: false,
-    isInventoryOpen: false,
+const state = {
     isGameRunning: false,
-    isGamePaused: false,
-    isPlayerTalking: false,
+    isInventoryOpen: false,
+    isActivityRunning: false,
+    isActivityBugRunning: false,
+    isActivityBugCooldown: false,
 }
+
+export const setState = (key: keyof typeof state, value: boolean) => state[key] = value;
+export const getState = (key: keyof typeof state) => state[key];
