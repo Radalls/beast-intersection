cbb7f9e8b6725fc35216359a593007afcc9686af
Radalls <leosallard@gmail.com>
2024-03-06 21:24:56 +0100

    fix: Engine Entity - remove ids

--- src/engine/entities/entity.ts
+++ src/engine/entities/entity.ts
@@ -3,6 +3,3 @@ import { Component } from "../components/component";
 export type Entity = {
     [K in keyof Component]: Component[K] | undefined;
 };
-
-export const TILEMAP_ENTITY_ID = 'TileMap';
-export const PLAYER_ENTITY_ID = 'Player';
