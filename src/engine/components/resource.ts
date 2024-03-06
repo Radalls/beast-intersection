5d4b29fed1384967a1149ae13982cc0a4fca5778
Radalls <leosallard@gmail.com>
2024-03-06 21:12:09 +0100

    feat: Resource Component - add activity bug data

--- src/engine/components/resource.ts
+++ src/engine/components/resource.ts
@@ -2,6 +2,25 @@ import { Item } from "./inventory";
 
 export type Resource = {
     _: 'Resource',
+    _activityType: ActivityTypes,
     _isTemporary: boolean,
+    activityData?: ActivityBugData,
     item: Item,
 };
+
+export enum ActivityTypes {
+    PICKUP = 'PICKUP',
+    BUG = 'BUG',
+}
+
+export type ActivityData = ActivityBugData;
+
+export type ActivityBugData = {
+    _hp: number,
+    _maxHp: number,
+    _maxNbErrors: number,
+    _nbErrors: number,
+    _symbol?: string,
+    _symbolInterval: number,
+    _symbolFound?: boolean,
+}
