a359dcf0150de04deca15c760064f2c303fcb948
Radalls <leosallard@gmail.com>
2024-03-07 21:16:28 +0100

    feat: Engine Cycle - add main activity cycle

--- src/engine/cycle.ts
+++ src/engine/cycle.ts
@@ -1,15 +1,18 @@
 import { getState, setState } from "./state"
 import { tickActivityBug } from "./services/activity/activity.bug";
+import { endActivity } from "./services/activity/activity";
 
 const cycle = {
     deltaTime: 1 / 60,
     elapsedGameTime: 0,
     elapsedActivityTime: 0,
+    activityInterval: 0,
     activityBugTickInterval: 0,
 }
 
 export const setCycle = (key: keyof typeof cycle, value: number) => cycle[key] = value;
 export const getCycle = (key: keyof typeof cycle) => cycle[key];
+export const clearCycle = (key: keyof typeof cycle) => cycle[key] = 0;
 
 export const runCycle = () => {
     if (getState('isGameRunning')) {
@@ -29,6 +32,15 @@ export const runCycle = () => {
                 if (getState('isActivityBugCooldown')) {
                     setState('isActivityBugCooldown', false);
                 }
+            }
+        }
+
+        if (getState('isActivityWin') && cycle.activityInterval > 0) {
+            const activityIntervals = Math.floor(cycle.elapsedActivityTime / cycle.activityInterval);
+
+            if (activityIntervals > 0) {
+                setState('isActivityWin', false);
+                endActivity({});
             }
         }
     }
