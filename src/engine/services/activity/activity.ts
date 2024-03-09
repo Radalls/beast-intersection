cfb3e41632d9f4cc3cb8a15c2f0603d23edf10bb
Radalls <leosallard@gmail.com>
2024-03-09 19:51:58 +0100

    feat: Activity Service - add helpers + add regions

--- src/engine/services/activity/activity.ts
+++ src/engine/services/activity/activity.ts
@@ -1,49 +1,53 @@
-import { event } from "../../../render/event";
-import { clearCycle, setCycle } from "../../cycle";
+import { event } from "../../../render/events/event";
+import { setCycle } from "../../cycle";
 import { EventTypes } from "../../event";
 import { setState } from "../../state";
 import { getStore, clearStore, setStore } from "../../store";
 import { destroyResource, getResourceItem } from "../../systems/resource/resource";
 import { getComponent } from "../entity";
+import { error } from "../error";
 
+//#region HELPERS
+export const checkActivityId = ({ activityId }: { activityId: string | null | undefined }) => {
+    if (!(activityId)) activityId = getStore('activityId')
+        ?? error({ message: 'Store activityId is undefined', where: checkActivityId.name });
+
+    return activityId;
+}
+//#endregion
+
+//#region SERVICES
 export const startActivity = ({ activityId }: { activityId: string }) => {
-    setCycle('elapsedActivityTime', 0);
-    setCycle('activityInterval', 3);
+    setCycle('activityWinTickInterval', 1);
     setState('isActivityRunning', true);
     setStore('activityId', activityId);
+
+    event({
+        type: EventTypes.ACTIVITY_START,
+        entityId: activityId,
+    });
 }
 
 export const winActivity = ({ activityId }: { activityId?: string | null }) => {
-    activityId = activityId || getStore('activityId');
-    if (!(activityId)) {
-        throw {
-            message: `Resource activity does not exist`,
-            where: winActivity.name,
-        }
-    }
+    activityId = checkActivityId({ activityId });
 
-    const item = getResourceItem({ resourceEntityId: activityId });
+    const activityResource = getComponent({ entityId: activityId, componentId: 'Resource' });
+    getResourceItem({ resourceEntityId: activityId });
 
     setState('isActivityWin', true);
 
     event({
         type: EventTypes.ACTIVITY_WIN,
         entityId: activityId,
-        data: item,
+        data: activityResource,
     });
 }
 
 export const endActivity = ({ activityId }: { activityId?: string | null }) => {
-    activityId = activityId || getStore('activityId');
-    if (!(activityId)) {
-        throw {
-            message: `Resource activity does not exist`,
-            where: endActivity.name,
-        }
-    }
+    activityId = checkActivityId({ activityId });
 
     setState('isActivityRunning', false);
-    clearCycle('elapsedActivityTime');
+    setState('isActivityWin', false);
     clearStore('activityId');
 
     event({
@@ -56,3 +60,4 @@ export const endActivity = ({ activityId }: { activityId?: string | null }) => {
         destroyResource({ resourceEntityId: activityId });
     }
 }
+//#endregion
