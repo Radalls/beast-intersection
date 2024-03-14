56837e92fe6d58e41693416a64d5856078c5f419
Radalls <leosallard@gmail.com>
2024-03-14 09:57:44 +0100

    core: add dialog styles to app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { main as renderMain } from "./render/main";
 import './render/styles/activity.css';
 import './render/styles/inventory.css';
 import './render/styles/tilemap.css';
+import './render/styles/dialog.css';
 
 renderMain();
 engineMain();
