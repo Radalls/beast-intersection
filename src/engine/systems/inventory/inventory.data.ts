c7ecb6eb985e565d1a657debea79e31d152c42fb
Radalls <leosallard@gmail.com>
2024-03-29 17:56:42 +0100

    fix: Inventory System - refactor item rules to json

--- src/engine/systems/inventory/inventory.data.ts
+++ src/engine/systems/inventory/inventory.data.ts
@@ -1,31 +1,3 @@
-type InventoryData = {
-    itemRules: ItemRule[];
-};
+import itemRules from '../../../assets/items/item_rules.json';
 
-type ItemRule = {
-    itemName: 'Wood1' | 'Bug1' | 'Bug2' | 'Fish1';
-    maxAmount: number;
-};
-
-export const findItemRule = (itemName: string) => inventoryData.itemRules.find((rule) => rule.itemName === itemName);
-
-export const inventoryData: InventoryData = {
-    itemRules: [
-        {
-            itemName: 'Wood1',
-            maxAmount: 5,
-        },
-        {
-            itemName: 'Bug1',
-            maxAmount: 1,
-        },
-        {
-            itemName: 'Bug2',
-            maxAmount: 1,
-        },
-        {
-            itemName: 'Fish1',
-            maxAmount: 1,
-        }
-    ]
-}
+export const findItemRule = (itemName: string) => itemRules.find((rule) => rule.name === itemName);
