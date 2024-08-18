78f4b888d7e28031e9d8e206506e2dbcee31831d
Radalls <leosallard@gmail.com>
2024-08-18 20:44:13 +0200

    fix: Dialog Component - fix lint

--- src/engine/components/dialog.ts
+++ src/engine/components/dialog.ts
@@ -1,15 +1,15 @@
 export type Dialog = {
     _: 'Dialog';
     _currentId?: DialogText['_id'];
-    _currentValue?: DialogText['_value'];
     _currentOptionIndex?: number;
     _currentOptionsValues?: DialogText['_value'][];
+    _currentValue?: DialogText['_value'];
     texts: DialogText[];
 };
 
 export type DialogText = {
     _id: number;
-    _value: string;
     _next?: DialogText['_id'];
     _options: DialogText['_id'][];
+    _value: string;
 };
