138dde8cbb60a3ccb3dd54964acbd364b512ca3d
Radalls <leosallard@gmail.com>
2024-08-18 20:44:02 +0200

    fix: Component - fix lint

--- src/engine/components/@component.ts
+++ src/engine/components/@component.ts
@@ -1,11 +1,11 @@
-import { Collider } from "./collider";
-import { Dialog } from "./dialog";
-import { Inventory } from "./inventory";
-import { Position } from "./position";
-import { Resource } from "./resource";
-import { Sprite } from "./sprite";
-import { TileMap } from "./tilemap";
-import { Trigger } from "./trigger";
+import { Collider } from './collider';
+import { Dialog } from './dialog';
+import { Inventory } from './inventory';
+import { Position } from './position';
+import { Resource } from './resource';
+import { Sprite } from './sprite';
+import { TileMap } from './tilemap';
+import { Trigger } from './trigger';
 
 export type Component = {
     'Collider': Collider;
