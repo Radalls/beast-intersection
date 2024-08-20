b9967fb508ab155e962e4dd521697469bf7e9635
Radalls <leosallard@gmail.com>
2024-08-20 11:49:02 +0200

    feat: Tilemap System - add utils file

---
+++ src/engine/systems/tilemap/tilemap.utils.ts
@@ -0,0 +1,24 @@
+import { getComponent } from '@/engine/entities/entity.manager';
+import { error } from '@/engine/services/error';
+import { getStore } from '@/engine/store';
+
+//#region UTILS
+export const findTileByEntityId = ({ entityId }: { entityId: string }) => {
+    const tilemapEntityId = getStore('tilemapId')
+        ?? error({ message: 'Store tilemapId is undefined', where: findTileByEntityId.name });
+
+    return getComponent({ componentId: 'TileMap', entityId: tilemapEntityId })
+        .tiles.find(tile => tile._entityIds.includes(entityId));
+};
+
+export const findTileByPosition = ({ x, y }: {
+    x: number,
+    y: number,
+}) => {
+    const tilemapEntityId = getStore('tilemapId')
+        ?? error({ message: 'Store tilemapId is undefined', where: findTileByPosition.name });
+
+    return getComponent({ componentId: 'TileMap', entityId: tilemapEntityId })
+        .tiles.find(tile => tile.position._x === x && tile.position._y === y);
+};
+//#endregion
